d174ee5fec9e9671aae5942bbc91e59d502e2cb1
Vlady <vlady@onlinedata.dk>
2023-01-31 17:07:46 +0100

    Added google analytics, fixed button size

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,6 +4,21 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-BNEMTBTDEH`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', 'G-BNEMTBTDEH');
+          `,
+          }}
+        />
         <meta name="author" content="" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
