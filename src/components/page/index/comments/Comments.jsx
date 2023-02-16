9069ff35137f5bb05ea5c0f4b485736a399d2ce2
Vlady <vlady@onlinedata.dk>
2023-02-16 14:04:08 +0100

    Reworked comments section

--- src/components/page/index/comments/Comments.jsx
+++ src/components/page/index/comments/Comments.jsx
@@ -23,11 +23,70 @@ import { useTranslation } from "react-i18next";
 
 const Comments = () => {
   const { t, i18n } = useTranslation();
+
+  const comments = [
+    {
+      text: t(
+        "It's very convenient to use ITinder because job search and communication with recruiters are done simply on the phone."
+      ),
+      name: t("Maria T."),
+      position: "SMM manager",
+      stars: [1, 1, 1, 1, 0.5],
+      photo: Comment1Img.src,
+    },
+    {
+      text: t(
+        "A great bot for finding IT jobs. Everything is clear and understandable🙂"
+      ),
+      name: t("Evgenia Syniavska"),
+      position: "IT Researcher",
+      stars: [1, 1, 1, 1, 1],
+      photo: Comment2Img.src,
+    },
+    {
+      text: t(
+        "I really liked using the bot, it was super easy, fast, and convenient! The whole experience was smooth and user-friendly. I'll definitely use the service again!"
+      ),
+      name: t("Yel Malichenko"),
+      position: "Project manager",
+      stars: [1, 1, 1, 1, 0],
+      photo: Comment3Img.src,
+    },
+    {
+      text: t(
+        "It was great to have the position closed so quickly.🤩 My recommendations for everyone! I appreciate your help a lot 👍"
+      ),
+      name: t("Elizabeth Zhyrova"),
+      position: "Recruitment Researcher",
+      stars: [1, 1, 1, 1, 1],
+      photo: Comment4Img.src,
+    },
+    {
+      text: t(
+        "ITinder is an amazing bot. It's easy to use. I received really cool candidates, who went through the entire hiring process. I actively use it in our work, and totally recommend it!"
+      ),
+      name: t("Yana Martyniuk"),
+      position: "IT Recruiter",
+      stars: [1, 1, 1, 1, 1],
+      photo: Comment5Img.src,
+    },
+    {
+      text: t(
+        "Simple and intuitive service. Much easier access than other services, though brings the best features from job seeker standpoint, especially the salary filter, which is the best."
+      ),
+      name: t("Sergiy Khomchenko"),
+      position: "Senior .NET Software Engineer",
+      stars: [1, 1, 1, 1, 1],
+      photo: Comment6Img.src,
+    },
+  ];
+
   return (
     <Container className={classes.container} fluid>
       <Row className={"text-center " + classes.title_container}>
         <h2>{t("What users say about ITinder")}</h2>
       </Row>
+
       <Row className={classes.comments_container}>
         <Swiper
           spaceBetween={50}
@@ -39,202 +98,40 @@ const Comments = () => {
           modules={[Pagination, Navigation]}
           className={classes.swiper}
         >
-          <SwiperSlide className={classes.swiper_slide}>
-            <div className={classes.stars}>
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <HalfStar className={classes.star} />
-            </div>
-            <p className={classes.comment_text}>
-              “
-              {t(
-                "It's very convenient to use ITinder because job search and communication with recruiters are done simply on the phone."
-              )}
-              ”
-            </p>
-            <div className={classes.commentor}>
-              <Image
-                src={Comment1Img.src}
-                className={classes.commentor_photo}
-                loading="lazy"
-                alt="ITinder SMM manager Maria T."
-                width={47}
-                height={47}
-              />
-              <div>
-                <p className={classes.commentor_name}>{t("Maria T.")}</p>
-                <p className={classes.commentor_position}>SMM manager</p>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={classes.swiper_slide}>
-            <div className={classes.stars}>
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-            </div>
-            <p className={classes.comment_text}>
-              “
-              {t(
-                "A great bot for finding IT jobs. Everything is clear and understandable🙂"
-              )}
-              ”
-            </p>
-            <div className={classes.commentor}>
-              <Image
-                src={Comment2Img.src}
-                className={classes.commentor_photo}
-                loading="lazy"
-                alt="ITinder IT Researcher Evgenia Syniavska"
-                width={47}
-                height={47}
-              />
-              <div>
-                <p className={classes.commentor_name}>
-                  {t("Evgenia Syniavska")}
-                </p>
-                <p className={classes.commentor_position}>IT Researcher</p>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={classes.swiper_slide}>
-            <div className={classes.stars}>
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <EmptyStar className={classes.star} />
-            </div>
-            <p className={classes.comment_text}>
-              “
-              {t(
-                "I really liked using the bot, it was super easy, fast, and convenient! The whole experience was smooth and user-friendly. I'll definitely use the service again!"
-              )}
-              ”
-            </p>
-            <div className={classes.commentor}>
-              <Image
-                src={Comment3Img.src}
-                className={classes.commentor_photo}
-                loading="lazy"
-                alt="ITinder Project manager Yel Malichenko"
-                width={47}
-                height={47}
-              />
-              <div>
-                <p className={classes.commentor_name}>{t("Yel Malichenko")}</p>
-                <p className={classes.commentor_position}>Project manager</p>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={classes.swiper_slide}>
-            <div className={classes.stars}>
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-            </div>
-            <p className={classes.comment_text}>
-              “
-              {t(
-                "It was great to have the position closed so quickly.🤩 My recommendations for everyone! I appreciate your help a lot 👍"
-              )}
-              ”
-            </p>
-            <div className={classes.commentor}>
-              <Image
-                src={Comment4Img.src}
-                className={classes.commentor_photo}
-                loading="lazy"
-                alt="ITinder Elizabeth Zhyrova Recruitment Researcher"
-                width={47}
-                height={47}
-              />
-              <div>
-                <p className={classes.commentor_name}>
-                  {t("Elizabeth Zhyrova")}
-                </p>
-                <p className={classes.commentor_position}>
-                  Recruitment Researcher
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={classes.swiper_slide}>
-            <div className={classes.stars}>
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-            </div>
-            <p className={classes.comment_text}>
-              “
-              {t(
-                "ITinder is an amazing bot. It's easy to use. I received really cool candidates, who went through the entire hiring process. I actively use it in our work, and totally recommend it!"
-              )}
-              ”
-            </p>
-            <div className={classes.commentor}>
-              <Image
-                src={Comment5Img.src}
-                className={classes.commentor_photo}
-                loading="lazy"
-                alt="ITinder Yana Martyniuk IT Recruiter"
-                width={47}
-                height={47}
-              />
-              <div>
-                <p className={classes.commentor_name}>{t("Yana Martyniuk")}</p>
-                <p className={classes.commentor_position}>IT Recruiter</p>
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide className={classes.swiper_slide}>
-            <div className={classes.stars}>
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-              <FullStar className={classes.star} />
-            </div>
-            <p className={classes.comment_text}>
-              “
-              {t(
-                "Simple and intuitive service. Much easier access than other services, though brings the best features from job seeker standpoint, especially the salary filter, which is the best."
-              )}
-              ”
-            </p>
-            <div className={classes.commentor}>
-              <Image
-                src={Comment6Img.src}
-                className={classes.commentor_photo}
-                loading="lazy"
-                alt="ITinder Senior .NET Software Engineer Sergiy Khomchenko"
-                width={47}
-                height={47}
-                quality={75}
-              />
-              <div>
-                <p className={classes.commentor_name}>
-                  {t("Sergiy Khomchenko")}
-                </p>
-                <p className={classes.commentor_position}>
-                  Senior .NET Software Engineer
-                </p>
-              </div>
-            </div>
-          </SwiperSlide>
+          {comments.map((item, index) => {
+            return (
+              <SwiperSlide className={classes.swiper_slide} key={index}>
+                <div className={classes.stars}>
+                  {item.stars.map((star, index) => {
+                    return star == 1 ? (
+                      <FullStar className={classes.star} key={index} />
+                    ) : star == 0.5 ? (
+                      <HalfStar className={classes.star} key={index} />
+                    ) : (
+                      <EmptyStar className={classes.star} key={index} />
+                    );
+                  })}
+                </div>
+                <p className={classes.comment_text}>“{item.text}”</p>
+                <div className={classes.commentor}>
+                  <Image
+                    src={item.photo}
+                    className={classes.commentor_photo}
+                    loading="lazy"
+                    alt={"ITinder " + item.name}
+                    width={47}
+                    height={47}
+                  />
+                  <div>
+                    <p className={classes.commentor_name}>{item.name}</p>
+                    <p className={classes.commentor_position}>
+                      {item.position}
+                    </p>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
         </Swiper>
         <div className={classes.back_left}></div>
         <div className={classes.back_right}></div>
